6b1dd0829e78a9ef3ead3b5437a481108baab425
dhqdqk <1901839966@qq.com>
2017-10-09 05:25:14 +0800

    1.0

---
+++ mysoft.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# apt install
+sudo apt install python3-pip
+sudo apt install pyspider3
+sudo apt install phantomjs
+sudo apt install jupyter-notebook
+sudo apt install emacs
+sudo apt install shutter
+sudo apt install libssl-dev
+sudo apt install synaptic
+sudo apt install workrave
+sudo apt install goldendict
+
+# python packages
+sudo pip3 install virturalenv
+sudo pip3 install pyopenssl
+sudo pip3 install pycurl
+sudo pip3 install Scrapy
+sudo pip3 install selenium
+sudo pip install PySide
+
+# golang
+sudo apt install golang
+
+
